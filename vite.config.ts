28f1a9cd49c117c748bcd4603f42c2e48c9c1fbd
Ant-Tonmay <tonmaysardar500@gmail.com>
2026-07-04 13:41:54 +0530

    change the domain name

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,6 @@ function artifactsPlugin(): Plugin {
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/aham.dev/',   
+  base: '/ahamlang.tech/',   
   plugins: [react(), artifactsPlugin()],
 })
